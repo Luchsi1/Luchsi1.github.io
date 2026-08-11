52c1c5e171e1a9bbe3fa69a7ce726aa7b43f2307
Luchsi1 <136190833+Luchsi1@users.noreply.github.com>
2026-08-11 11:59:37 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,43 @@
-function hello() {
-    alert("Hello!");
+const regions = await fetch("regions.json")
+    .then(response => response.json());
+
+const countries = await fetch("options/regions/world.json")
+    .then(response => response.json());
+
+const region = regions
+
+nextRegion(region)
+
+function nextRegion(region) {
+
+    const buttons = document.getElementById("buttons");
+    buttons.innerHTML = "";
+
+    for (const newRegion of Object.values(region)) {
+
+        if (typeof newRegion !== "object") {
+            continue;
+        }
+
+        const button = document.createElement("button");
+
+        button.className = "menu-button";
+        button.textContent = newRegion.name;
+
+        button.addEventListener("click", () => {
+            changeRegion(newRegion);
+        });
+
+        buttons.appendChild(button);
+    }
+}
+
+function changeRegion(newRegion) {
+
+    currentRegion = newRegion;
+
+    document.getElementById("current-selection").textContent =
+        newRegion.name;
+
+    nextRegion(newRegion);
 }
